d616ed9e199e5a2ad6800774420c066835afcef5
Tanner Foster <tannerfoster@ferristexas.gov>
2025-01-18 02:05:51 -0600

    Lab Functions in JS Submission

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 
 
+function calculateTax(amount) {
+    console.log( amount * .10)
+}
 
+function convertToUpperCase(text) {
+    console.log(text.toUpperCase())
+}
+
+function findMaximum(num1, num2) {
+    if(num1 > num2){
+        console.log(num1)
+    }else {
+        console.log(num2)
+    }
+}
+
+function isPalindrome(word) {
+    const wordreverse = word.split('').reverse().join('');
+    if(word === wordreverse) {
+        console.log(true)
+    }else {
+        console.log(false)
+    }
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    const discountedAmount = originalPrice * discountPercentage;
+
+    console.log(originalPrice - discountedAmount);
+}
 
 
 // This is required for the test to function properly  
